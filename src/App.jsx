d51ebca6f81f63849e484e4a2fba43cd906b4803
Tashi D. Gyeltshen <tashidgyeltshen@hotmail.com>
2024-11-06 11:10:17 -0500

    Place the Navbar and Footer in the components directory, and use a route outlet to configure the route layout.

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import About from './pages/About';
-import Footer from './pages/Footer';
 import Home from './pages/Home';
-import Navbar from './pages/Navbar';
+import Layout from './components/Layout';
 import VanDetail from './pages/VanDetail';
 import Vans from './pages/Vans';
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/vans' element={<Vans />} />
-        <Route path='/vans/:id' element={<VanDetail />} />
+        <Route element={<Layout />}>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/vans' element={<Vans />} />
+          <Route path='/vans/:id' element={<VanDetail />} />
+        </Route>
       </Routes>
-      <Footer />
     </BrowserRouter>
   )
 }
