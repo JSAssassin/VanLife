85ca0d1fbb9da78c6a444196dfed6374a390613f
Tashi D. Gyeltshen <tashidgyeltshen@hotmail.com>
2024-11-09 13:48:45 -0500

    Add `HostVanDetailLayout` component.

--- src/components/HostVanDetailLayout.jsx
+++ src/components/HostVanDetailLayout.jsx
@@ -1,11 +1,28 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useParams } from 'react-router-dom';
 import BackLink from './BackLink';
+import HostVanDetailCard from './HostVanDetailCard';
+import React from 'react';
+import './HostVanDetailLayout.css';
+
+const HostVanDetailContext = React.createContext();
 
 export default function HostVanDetailLayout() {
+    const { id: vanId } = useParams();
+    const [hostVan, setHostVan] = React.useState({});
+    React.useEffect(() => {
+        fetch(`/api/host/vans/${vanId}`)
+            .then(res => res.json())
+            .then(data => setHostVan(data.vans[0]))
+    }, [vanId])
     return (
-        <>
-            <BackLink linkTo='/host/vans'/>
-            <Outlet />
-        </>
+        <HostVanDetailContext.Provider value={hostVan}>
+            <div className='host-van-detail-layout'>
+                <BackLink linkTo='/host/vans'/>
+                <HostVanDetailCard showType={true} hostVan={hostVan} className='host-vans__card-detail-layout' />
+                <Outlet />
+            </div>
+        </HostVanDetailContext.Provider>
     );
 }
+
+export { HostVanDetailContext };
