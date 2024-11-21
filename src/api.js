35559601b8892b012b7fe5653dc19acb64eecacf
Tashi D. Gyeltshen <tashidgyeltshen@hotmail.com>
2024-11-20 22:55:29 -0500

    Update the APIs to require host ID and add new `loginUser()` API.

--- src/api.js
+++ src/api.js
@@ -31,20 +31,20 @@ export async function getVan(id) {
     return data.vans;
 }
 
-export async function getHostVan(id) {
-    const res = await fetch(`/api/host/vans/${id}`);
+export async function getHostVan({hostId, vanId}) {
+    const res = await fetch(`/api/host/${hostId}/vans/${vanId}`);
     if (!res.ok) {
         throw new FetchError('Failed to fetch host van', res.status, res.statusText);
     }
     const data = await res.json();
     if (!data || !data.vans) {
-        throw new Error(`Host van with ID "${id}" not found`);
+        throw new Error(`Host van with ID "${vanId}" not found`);
     }
     return data.vans;
 }
 
-export async function getHostVans() {
-    const res = await fetch('/api/host/vans');
+export async function getHostVans({hostId}) {
+    const res = await fetch(`/api/host/${hostId}/vans`);
     if (!res.ok) {
         throw new FetchError('Failed to fetch host vans', res.status, res.statusText);
     }
@@ -53,4 +53,19 @@ export async function getHostVans() {
         throw new Error('Failed to fetch host vans');
     }
     return data.vans;
+}
+
+export async function loginUser(creds) {
+    const res = await fetch('/api/login', {
+        method: "post",
+        body: JSON.stringify(creds),
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    });
+    const data = await res.json();
+    if (!res.ok) {
+        throw new FetchError(data.message, res.status, res.statusText);
+    }
+    return data;
 }
