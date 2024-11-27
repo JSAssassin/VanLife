07d0e0bbef9bc20f891f2e13b9abe2978fff7ad0
Tashi D. Gyeltshen <tashidgyeltshen@hotmail.com>
2024-11-27 09:36:44 -0500

    Add login and logout functionality.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,44 @@
-import { NavLink, Link } from 'react-router-dom';
-import navbarLogo from '../assets/logo.png'
+import { useState, useEffect } from 'react';
+import { NavLink, Link, useNavigate } from 'react-router-dom';
+import { IoMdLogOut } from "react-icons/io";
+import navbarLogo from '../assets/logo.png';
+import avatarIcon from '../assets/avatar-icon.png';
+
 import './Navbar.css';
 
+function useForceUpdate() {
+    const [, setState] = useState(0);
+    return () => setState(state => state + 1);
+}
+
 export default function Navbar() {
+    const [loggedIn, setLoggedIn] = useState(false);
+    const navigate = useNavigate();
+    const forceUpdate = useForceUpdate();
+    useEffect(() => {
+        const loggedInStatus = localStorage.getItem("loggedin");
+        setLoggedIn(loggedInStatus ? true : false);
+
+        const handleStorageChange = () => {
+            const loggedInStatus = localStorage.getItem("loggedin");
+            setLoggedIn(loggedInStatus ? true : false);
+            forceUpdate();
+        };
+
+        window.addEventListener('storage', handleStorageChange);
+
+        return () => {
+            window.removeEventListener('storage', handleStorageChange);
+        };
+    }, [forceUpdate]);
+
+    function handleFakeLogout() {
+        localStorage.removeItem("loggedin");
+        localStorage.removeItem("user");
+        setLoggedIn(false);
+        navigate('/');
+    }
+
     return (
         <nav className="navbar">
             <Link to="." className="navbar__logo">
@@ -27,6 +63,18 @@ export default function Navbar() {
                 >
                     Vans
                 </NavLink>
+                {
+                    loggedIn ? (
+                        <IoMdLogOut onClick={handleFakeLogout} className="navbar__item logout-btn" />
+                    ) : (
+                        <NavLink
+                            to="/login"
+                            className={({ isActive }) => `navbar__item ${isActive ? "active-link" : ""}`}
+                            >
+                            <img src={avatarIcon} alt="avatar-icon" width="20px"/>
+                        </NavLink>
+                    )
+                }
             </div>
         </nav>
     );
